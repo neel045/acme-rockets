2ace6e973c2fcdd8341a91f9cef832c6a5eaee77
Neel Patel <patelneel0445@gmail.com>
2023-12-12 22:41:56 +0530

    lazy load and spinner and pwa made

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,40 @@
-import ContactUs from "./components/ContactUs";
-import Footer from "./components/Footer";
-import Hero from "./components/Hero";
-import Navbar from "./components/Navbar";
-import Rockets from "./components/Rockets";
-import Testimonials from "./components/Testimonials";
+import { Suspense, lazy } from "react";
+
+const ContactUs = lazy(() => import("./components/ContactUs"));
+const Footer = lazy(() => import("./components/Footer"));
+const Hero = lazy(() => import("./components/Hero"));
+const Navbar = lazy(() => import("./components/Navbar"));
+const Rockets = lazy(() => import("./components/Rockets"));
+const Testimonials = lazy(() => import("./components/Testimonials"));
 
 function App() {
   return (
     <div className="min-h-screen bg-slate-50 dark:bg-gray-800 dark:text-white">
-      <Navbar />
-      <main className="mx-auto max-w-4xl">
-        <Hero />
-        <hr className="mx-auto w-1/2 bg-gray-800 dark:bg-white" />
-        <Rockets />
-        <hr className="mx-auto w-1/2 bg-gray-800 dark:bg-white" />
-        <Testimonials />
-        <hr className="mx-auto w-1/2 bg-gray-800 dark:bg-white" />
-        <ContactUs />
-      </main>
-      <Footer />
+      <Suspense
+        fallback={
+          <div className=" flex justify-center items-center container">
+            <div
+              className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+              role="status"
+            >
+              <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"></span>
+            </div>
+          </div>
+        }
+      >
+        <Navbar />
+
+        <main className="mx-auto max-w-4xl">
+          <Hero />
+          <hr className="mx-auto w-1/2 bg-gray-800 dark:bg-white" />
+          <Rockets />
+          <hr className="mx-auto w-1/2 bg-gray-800 dark:bg-white" />
+          <Testimonials />
+          <hr className="mx-auto w-1/2 bg-gray-800 dark:bg-white" />
+          <ContactUs />
+        </main>
+        <Footer />
+      </Suspense>
     </div>
   );
 }
